8657e06a2fa6ede10b0852086ccc5e0a72cff933
trombonist76 <mahmut.hg@gmail.com>
2022-10-04 19:01:33 +0300

    [Update] Card component & card content updated.

--- src/components/CardContent/CardContent.vue
+++ src/components/CardContent/CardContent.vue
@@ -1,8 +1,19 @@
 <script setup>
-	
+import { ref } from "vue";
+import Card from "./Card.vue";
+const props = defineProps(["data"])
+const activeCard = ref(null)
+
 </script>
+
 <template>
-	<div>
-		CardContent
+	<div class="group">
+		<Card v-for="item in props.data" :key="item" :item="item"/>
 	</div>
 </template>
+
+<style scoped lang="scss">
+	.group{
+		@apply flex flex-wrap gap-[30px]
+	}
+</style>
