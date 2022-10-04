26ac60c7751027afd683ad82df6aa9acc294e020
trombonist76 <mahmut.hg@gmail.com>
2022-10-05 02:41:50 +0300

    [Update] ActiveCard feature added.

--- src/components/CardContent/Card.vue
+++ src/components/CardContent/Card.vue
@@ -5,19 +5,17 @@ import Plus from '../../assets/svg/Plus.vue';
 import More from '../../assets/svg/More.vue';
 import Status from './Status.vue';
 
-const props = defineProps(["item"])
+const props = defineProps(["item", "isActive"])
+const emits = defineEmits(["setActive"])
 const notOnHold = computed(() => props.item.status !== "On Hold")
 const dateLabel = computed(() => notOnHold.value ? "Start Date" : "Started")
 const progressWidth = computed(() => `${props.item.progress}%`)
-/**
- * * TODO proje title renkleri tailwindcss'e eklenecek #3C557A
- * * TODO icon renkleri tailwindcss'e eklenecek #8B8B8B
- * * TODO divider renk tailwind #CDDCEE
- * * Todo genel text #717986 main.scss global verilebilir color olarak
- */
+const setActiveHandler = () => {
+	emits("setActive", props.item.id)
+}
 </script>
 <template>
-	<div class="card">
+	<div class="card" :class="{'shadow-2xl border-none': isActive}" @click="setActiveHandler">
 		<div class="card__header">
 			<span class="title">{{item.title}}</span>
 			<div class="icons">
@@ -25,35 +23,35 @@ const progressWidth = computed(() => `${props.item.progress}%`)
 			</div>
 		</div>
 		<div v-show="notOnHold">
-			<Status class="description" :status="props.item.status" />
+			<Status class="boldie" :status="props.item.status" />
 		</div>
 		<div class="card__info">
-			<div class="date">
-				<h6 class="label">{{dateLabel}}</h6>
-				<span class="description">{{item.startDate}}</span>
+			<div>
+				<h6 class="thin">{{dateLabel}}</h6>
+				<span class="boldie">{{item.startDate}}</span>
 			</div>
-			<div v-show="!notOnHold" class="status">
-				<h6 class="label">Status</h6>
-				<Status class="description" :status="props.item.status" />
+			<div v-show="!notOnHold">
+				<h6 class="thin">Status</h6>
+				<Status class="boldie" :status="props.item.status" />
 			</div>
-			<div class="members">
-				<div class="members__badge">
-					<span class="value">
+			<div class="summary">
+				<div class="summary__badge">
+					<span class="stat">
 						{{item.tasks}}
 					</span>
-					<span class="label">Tasks</span>
+					<span class="thin">Tasks</span>
 				</div>
-				<div class="members__badge">
-					<span class="value">
+				<div class="summary__badge">
+					<span class="stat">
 						{{item.users}}
 					</span>
-					<span class="label">Members</span>
+					<span class="thin">Members</span>
 				</div>
 			</div>
 		</div>
 		<div class="card__members ">
 			<div class="header">
-				<span class="description">Members</span>
+				<span class="boldie">Members</span>
 			</div>
 			<div class="images">
 				<span v-for="member in item.members">
@@ -66,8 +64,8 @@ const progressWidth = computed(() => `${props.item.progress}%`)
 		</div>
 		<div class="progress">
 			<div class="progress__header">
-				<span class="description">Progress</span>
-				<span class="label">{{progressWidth}}</span>
+				<span class="boldie">Progress</span>
+				<span class="thin">{{progressWidth}}</span>
 			</div>
 			<div class="progress__bar">
 				<div class="width" :style="{width: progressWidth}"></div>
@@ -80,11 +78,10 @@ const progressWidth = computed(() => `${props.item.progress}%`)
 .card {
 	@apply w-[495px] h-[300px] border border-gray-dark p-6 flex flex-col justify-evenly rounded-lg gap-3;
 
-	.label {
+	.thin {
 		@apply text-xs;
 	}
-
-	.description {
+	.boldie {
 		@apply text-[13px] font-medium;
 	}
 
@@ -101,21 +98,17 @@ const progressWidth = computed(() => `${props.item.progress}%`)
 	}
 
 	&__info {
-		@apply flex justify-between text-center items-center mt-1.5;
+		@apply flex justify-between items-center mt-1.5;
 
 
-		.members {
-			@apply grid grid-cols-2 p-1 py-3.5 bg-gray text-center divide-x divide-divider w-[148px] rounded-md;
+		.summary {
+			@apply grid grid-cols-2 p-1 py-3.5 bg-gray-light text-center divide-x divide-divider w-[148px] rounded-md;
 
 			&__badge {
 				@apply flex flex-col;
 
-				.value {
+				.stat {
 					@apply text-lg text-navy font-bold;
-				}
-
-				.label {
-					@apply text-xs;
 				}
 			}
 		}
--- src/components/CardContent/CardContent.vue
+++ src/components/CardContent/CardContent.vue
@@ -3,12 +3,14 @@ import { ref } from "vue";
 import Card from "./Card.vue";
 const props = defineProps(["data"])
 const activeCard = ref(null)
-
+const activeCardHandler = (id) => {
+	activeCard.value = id
+}
 </script>
 
 <template>
 	<div class="group">
-		<Card v-for="item in props.data" :key="item" :item="item"/>
+		<Card v-for="item in props.data" :item="item" @setActive="activeCardHandler" :isActive="activeCard === item.id" :key="item.id" />
 	</div>
 </template>
 
