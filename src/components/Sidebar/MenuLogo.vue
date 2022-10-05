729f3b02dc14f460e44a48b2d298bd1b3f118fa6
trombonist76 <mahmut.hg@gmail.com>
2022-10-05 17:02:33 +0300

    [Update] Sidebar Logo created.

---
+++ src/components/Sidebar/MenuLogo.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Logo from '../../assets/svg/Logo.vue';
+
+</script>
+<template>
+	<div class="logo">
+		<Logo/>
+		<span class="brand">ChirKuut</span>
+	</div>
+</template>
+
+<style scoped>
+	.logo{
+		display: flex;
+		align-items: center;
+		gap: 5px;
+	}
+
+	.brand{
+		font-weight: bold;
+		font-size: 20px;
+		color: black;
+	}
+</style>
--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MenuItem from "./MenuItem.vue";
+import MenuLogo from "./MenuLogo.vue";
 const props = defineProps(["categories"]);
 </script>
 
 <template>
 	<div class="wrapper">
 		<div class="sidebar">
+			<MenuLogo/>
 			<div class="sidebar__menu">
 				<MenuItem
 					v-for="item in props.categories"
