d1ea1be7e42e00306276b8af2dfec0f2e2e7f87c
trombonist76 <mahmut.hg@gmail.com>
2022-10-06 03:55:44 +0300

    [Add] Card splitted much components etc Memberlist

--- src/components/CardGroup/CardGroup.vue
+++ src/components/CardGroup/CardGroup.vue
@@ -1,21 +1,21 @@
 <script setup>
-import { ref } from "vue";
-import Card from "./Card.vue";
-const props = defineProps(["data"])
-const activeCard = ref(null)
-const activeCardHandler = (id) => {
-	activeCard.value = id
-}
-</script>
-
-<template>
-	<div class="group">
-		<Card v-for="item in props.data" :item="item" @setActive="activeCardHandler" :isActive="activeCard === item.id" :key="item.id" />
-	</div>
-</template>
-
-<style scoped lang="scss">
-	.group{
-		@apply flex flex-wrap gap-[30px]
+	import { ref } from "vue";
+	import Card from "./Card.vue";
+	const props = defineProps(["data"])
+	const activeCard = ref(null)
+	const activeCardHandler = (id) => {
+		activeCard.value = id
 	}
-</style>
+	</script>
+	
+	<template>
+		<div class="group">
+			<Card v-for="item in props.data" :item="item" @setActive="activeCardHandler" :isActive="activeCard === item.id" :key="item.id" />
+		</div>
+	</template>
+	
+	<style scoped lang="scss">
+		.group{
+			@apply flex flex-wrap gap-[30px]
+		}
+	</style>
